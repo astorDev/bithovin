17a07688dbc3fe7a0e8e0a5a1612c4c8b0190299
Egor Tarasov <pavluchenk0@yandex.ru>
2024-05-20 19:30:12 +0400

    cross checked dotnet and dart

--- dotnet/tests/Secp256k1Should.cs
+++ dotnet/tests/Secp256k1Should.cs
@@ -15,9 +15,9 @@ public class Secp256k1Should
         var signature = Secp256K1.GenerateSignature(Message, privateKey).Printed();
 
         var publicKey = Secp256K1.PublicKeyFor(privateKey).Printed();
-        var verified = Secp256K1.VerifySignature(Message, signature, publicKey).Printed("verified");
+        var verificationResult = Secp256K1.VerifySignature(Message, signature, publicKey).Printed("verified");
 
-        verified.Should().BeTrue();
+        verificationResult.Valid.Should().BeTrue();
     }
 
     [TestMethod]
@@ -28,8 +28,46 @@ public class Secp256k1Should
         var signature = Secp256K1.GenerateSignature(Message, privateKey).Printed();
 
         var publicKey = Secp256K1.PublicKeyFor(privateKey).Printed();
-        var verified = Secp256K1.VerifySignature(Message, signature, publicKey).Printed("verified");
+        var verificationResult = Secp256K1.VerifySignature(Message, signature, publicKey).Printed("verified");
 
-        verified.Should().BeTrue();
+        verificationResult.Valid.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void VerifyFromOutside()
+    {
+        var publicKey = new BigPoint(
+          new ("21285371770960987179343891334477951184299084915952205959541518459254283849951"),
+          new ("22671947423549970796294697518695530648245966458430018258533359741779880876856")
+        );
+
+        var signature = new ECSignature(
+          new ("86058557054575571086167585107837415358165949453073739311202780092611888743858"),
+          new ("22076606289889717131594195825238606622326255596425005294818774860206769341915")
+        );
+
+        var message = "Hello from dart secp256k1!"u8.ToArray();
+
+        var verificationResult = Secp256K1.VerifySignature(message, signature, publicKey).Printed("verified");
+        verificationResult.Valid.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void VerifyInvalidFromOutside()
+    {
+        var publicKey = new BigPoint(
+          new ("21285371770960987179343891334477951184299084915952205959541518459254283849952"), //invalidly ends with 2
+          new ("22671947423549970796294697518695530648245966458430018258533359741779880876856")
+        );
+
+        var signature = new ECSignature(
+          new ("86058557054575571086167585107837415358165949453073739311202780092611888743858"),
+          new ("22076606289889717131594195825238606622326255596425005294818774860206769341915")
+        );
+
+        var message = "Hello from dart secp256k1!"u8.ToArray();
+
+        var verificationResult = Secp256K1.VerifySignature(message, signature, publicKey).Printed("verified");
+        verificationResult.Valid.Should().BeTrue();
     }
 }
