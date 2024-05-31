0bc3cf646fbe31de8a331fa68cbb6dab89bd2438
Egor Tarasov <pavluchenk0@yandex.ru>
2024-05-31 09:10:45 +0400

    dart formats

---
+++ dart/lib/ec_signature_format.dart
@@ -0,0 +1,31 @@
+import 'dart:typed_data';
+
+import 'package:bithovin/der.dart';
+import 'package:bithovin/hex.dart';
+import 'package:pointycastle/ecc/api.dart';
+
+class ECSignatureBytesStringFormat {
+  String Function(Uint8List) bytesToString;
+  Uint8List Function(String) stringToBytes;
+  Uint8List Function(ECSignature) signatureToBytes;
+  ECSignature Function(Uint8List) bytesToSignature;
+
+  ECSignatureBytesStringFormat({
+    required this.bytesToString,
+    required this.stringToBytes,
+    required this.signatureToBytes,
+    required this.bytesToSignature,
+  });
+
+  String export(ECSignature signature) => bytesToString(signatureToBytes(signature));
+  ECSignature import(String bytes) => bytesToSignature(stringToBytes(bytes));
+}
+
+class ECSignatureFormatRegistry {
+  static final ECSignatureBytesStringFormat sec1DerHex = ECSignatureBytesStringFormat(
+    bytesToString: Hex.encode,
+    stringToBytes: Hex.decode,
+    signatureToBytes: Sec1Der.encode,
+    bytesToSignature: Sec1Der.decode,
+  );
+}
